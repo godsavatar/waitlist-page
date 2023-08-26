7df4e61932eb7e6078fd80a1317c08785d6deb19
Daniel <57568959+godsavatar@users.noreply.github.com>
2023-08-26 09:08:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// let ham = document.querySelectorAll(".line")
+// let nav = document.querySelector(".nav-menu")
+
+
+
+
+
+// function clicked(){
+//     nav.classList.toggle("hide-menu")
+// }
+
+// for (let i = 0; i < ham.length; i++) {
+//    ham[i].addEventListener("click", clicked)
+    
+// }
+
+
+
+
+
+
